206d4b88862d219884844560f3fe0fa7d1e83590
Doooodie <unfair.slenderman@gmail.com>
2022-10-08 20:43:44 +0300

    refactor: almost all interactions from js to css

--- online-zoo/pages/donate/index.js
+++ online-zoo/pages/donate/index.js
@@ -2,37 +2,6 @@ const radioSelections = document.querySelectorAll('.radio-selection');
 const radioDollarIcons = document.querySelectorAll('.radio-dollar-icon');
 const radioTexts = document.querySelectorAll('.radio-text');
 const anotherAmount = document.querySelector('.another-amount');
-const dollars1000Selector = radioSelections[2];
-const dollars1000Icon = radioDollarIcons[2];
-const dollars1000Text = radioTexts[2];
-const dollars100Selector = radioSelections[5];
-const dollars100Icon = radioDollarIcons[5];
-const dollars100Text = radioTexts[5];
-
-function alertUser() {
-  alert(
-    `Радиокнопки "по умолчанию" соответствуют макетам, если обновить страницу с нужным viewport
-    (<1000px акцент на $100, >=1000px акцент на $1000)`
-  );
-}
-
-window.onload = alertUser;
-
-if (document.documentElement.clientWidth < 1000) {
-  dollars1000Selector.checked = false;
-  dollars1000Icon.classList.remove('radio-dollar-icon_checked');
-  dollars1000Text.classList.remove('radio-text_checked');
-  dollars100Selector.checked = true;
-  dollars100Icon.classList.add('radio-dollar-icon_checked');
-  dollars100Text.classList.add('radio-text_checked');
-} else {
-  dollars1000Selector.checked = true;
-  dollars1000Icon.classList.add('radio-dollar-icon_checked');
-  dollars1000Text.classList.add('radio-text_checked');
-  dollars100Selector.checked = false;
-  dollars100Icon.classList.remove('radio-dollar-icon_checked');
-  dollars100Text.classList.remove('radio-text_checked');
-}
 
 radioSelections.forEach((radio, i) => {
   radio.onchange = () => {
--- online-zoo/pages/main/index.js
+++ online-zoo/pages/main/index.js
@@ -7,26 +7,3 @@ const cardsDescriptionsHeadings = document.querySelectorAll(
 const cardsDescriptionsTexts = document.querySelectorAll(
   '.card-description-text'
 );
-
-petsCards.forEach((card, i) => {
-  card.onmouseover = () => {
-    card.classList.add('pets-card_hover');
-    petsCardsImgs[i].classList.add('pets-card-img_hover');
-    cardsBottoms[i].classList.add('card-bottom_hover');
-    cardsDescriptionsHeadings[i].classList.add(
-      'card-description-heading_hover'
-    );
-    cardsDescriptionsTexts[i].classList.add('card-description-heading_hover');
-  };
-  card.onmouseout = () => {
-    card.classList.remove('pets-card_hover');
-    petsCardsImgs[i].classList.remove('pets-card-img_hover');
-    cardsBottoms[i].classList.remove('card-bottom_hover');
-    cardsDescriptionsHeadings[i].classList.remove(
-      'card-description-heading_hover'
-    );
-    cardsDescriptionsTexts[i].classList.remove(
-      'card-description-heading_hover'
-    );
-  };
-});
