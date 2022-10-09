62b9fb3ffe041b202d01cc7d9a4d7713825eb932
Doooodie <unfair.slenderman@gmail.com>
2022-10-10 00:47:43 +0300

    feat: add testimonials carousel

--- online-zoo/pages/main/index.js
+++ online-zoo/pages/main/index.js
@@ -1,9 +1,24 @@
-const petsCards = document.querySelectorAll('.pets-card');
-const petsCardsImgs = document.querySelectorAll('.pets-card-img');
-const cardsBottoms = document.querySelectorAll('.card-bottom');
-const cardsDescriptionsHeadings = document.querySelectorAll(
-  '.card-description-heading'
-);
-const cardsDescriptionsTexts = document.querySelectorAll(
-  '.card-description-text'
-);
+const testimonialsCard = document.querySelectorAll('.testimonials-card');
+const testimonialsSlider = document.querySelector('.testimonials-slider');
+
+testimonialsSlider.oninput = function () {
+  const cardWidth = parseInt(getComputedStyle(testimonialsCard[0]).flexBasis);
+  const value = -cardWidth - 30;
+
+  for (let i = +this.min; i <= +this.max; i++) {
+    if (i === +this.value) {
+      testimonialsCard.forEach(card => {
+        card.style.transform = `translateX(${value * i}px)`;
+      });
+    }
+  }
+};
+
+window.onresize = function () {
+  if (document.documentElement.clientWidth < 1000) {
+    testimonialsCard.forEach(card => {
+      card.style.transform = `translateX(0px)`;
+    });
+    testimonialsSlider.value = 0;
+  }
+};
