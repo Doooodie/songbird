76b80f3c6a4a49ea492959db0dd3509d0189a3ba
Doooodie <unfair.slenderman@gmail.com>
2022-10-09 17:06:28 +0300

    feat: add donate amount validation

--- online-zoo/pages/donate/index.js
+++ online-zoo/pages/donate/index.js
@@ -3,14 +3,19 @@ const radioDollarIcons = document.querySelectorAll('.radio-dollar-icon');
 const radioTexts = document.querySelectorAll('.radio-text');
 const anotherAmount = document.querySelector('.another-amount');
 
+function activateRadioText(i) {
+  for (let k = 0; k < radioSelections.length; k++) {
+    radioDollarIcons[k].classList.remove('radio-dollar-icon_checked');
+    radioTexts[k].classList.remove('radio-text_checked');
+  }
+  radioDollarIcons[i].classList.add('radio-dollar-icon_checked');
+  radioTexts[i].classList.add('radio-text_checked');
+}
+
 radioSelections.forEach((radio, i) => {
   radio.onchange = () => {
-    for (let k = 0; k < radioSelections.length; k++) {
-      radioDollarIcons[k].classList.remove('radio-dollar-icon_checked');
-      radioTexts[k].classList.remove('radio-text_checked');
-    }
-    radioDollarIcons[i].classList.add('radio-dollar-icon_checked');
-    radioTexts[i].classList.add('radio-text_checked');
+    activateRadioText(i);
+    anotherAmount.value = radioTexts[i].innerHTML;
   };
 });
 
@@ -18,4 +23,15 @@ anotherAmount.oninput = function () {
   if (this.value.length > this.maxLength) {
     this.value = this.value.slice(0, this.maxLength);
   }
+
+  radioTexts.forEach((text, i) => {
+    if (text.innerHTML == this.value) {
+      radioSelections[i].checked = true;
+      activateRadioText(i);
+    } else {
+      radioSelections[i].checked = false;
+      radioDollarIcons[i].classList.remove('radio-dollar-icon_checked');
+      text.classList.remove('radio-text_checked');
+    }
+  });
 };
