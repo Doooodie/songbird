c1e7678e78b61526d1fdde8ca0b00720c5ac4b30
Doooodie <unfair.slenderman@gmail.com>
2022-11-19 23:41:46 +0300

    feat: add next level button

--- songbird/src/pages/index/scripts/index.js
+++ songbird/src/pages/index/scripts/index.js
@@ -2,7 +2,6 @@ import 'modern-normalize/modern-normalize.css';
 import '../../../assets/styles/colors.css';
 import '../../../assets/styles/global.css';
 import '../../../assets/styles/header.css';
-import '../../../assets/styles/main.css';
 import '../../../assets/styles/footer.css';
 import '../styles/index.css';
 
--- songbird/src/pages/results/scripts/results.js
+++ songbird/src/pages/results/scripts/results.js
@@ -2,6 +2,5 @@ import 'modern-normalize/modern-normalize.css';
 import '../../../assets/styles/colors.css';
 import '../../../assets/styles/global.css';
 import '../../../assets/styles/header.css';
-import '../../../assets/styles/main.css';
 import '../../../assets/styles/footer.css';
 import '../styles/results.css';
