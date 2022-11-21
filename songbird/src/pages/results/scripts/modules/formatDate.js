d28673e6eb531459777dacf96274acea6eec2148
Doooodie <unfair.slenderman@gmail.com>
2022-11-21 04:50:07 +0300

    fix: results not render properly

--- songbird/src/pages/results/scripts/modules/formatDate.js
+++ songbird/src/pages/results/scripts/modules/formatDate.js
@@ -1,20 +1,13 @@
 export default function formatDate(date) {
-  const diff = new Date() - date;
-  const sec = Math.floor(diff / 1000);
-  const min = Math.floor(diff / 60000);
+  const options = {
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    hour12: false,
+  };
 
-  if (diff < 1000) return 'прямо сейчас';
-  if (sec < 60) return `${sec} сек. назад`;
-  if (min < 60) return `${min} мин. назад`;
-
-  let d = date;
-  d = [
-    `0 ${d.getDate()}`,
-    `0 ${d.getMonth() + 1}`,
-    ` ${d.getFullYear()}`,
-    `0 ${d.getHours()}`,
-    `0 ${d.getMinutes()}`,
-  ].map((component) => component.slice(-2));
-
-  return `${d.slice(0, 3).join('.')} ${d.slice(3).join(':')}`;
+  return new Date(date).toLocaleString('ru-RU', options);
 }
